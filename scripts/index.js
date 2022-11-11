12f78431a5a3312a4343a4077ab2a9bf0d9325c4
willyongaks <william.ongaki10@gmail.com>
2022-11-11 10:39:10 +0100

    question 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-import './question-1/question-1';
-import './question-2/question-2';
-import './question-3/question-3';
-import './question-4/question-4';
-import './question-5/question-5';
+import './question-1/question-1.js';
+import './question-2/question-2.js';
+import './question-3/question-3.js';
+import './question-4/question-4.js';
+import './question-5/question-5.js';
 
 
 
--- scripts/question-1/question-1.ts
+++ scripts/question-1/question-1.ts
@@ -8,7 +8,7 @@ console.log("wlliam is here")
 //  age: number
 //  isAdmin: OPTIONAL boolean
 
-interface createUser {
+interface User {
   school: string;
   firstName: string;
   lastName: string;
@@ -22,17 +22,19 @@ interface createUser {
 //  User interface as the return type
 
 
-function createUser(firstName, lastName, age, isAdmin = false) {
+function createUser(firstName, lastName, age, isAdmin = false): User {
   return {
     school: 'Noroff',
-    firstName: "String",
-    lastName: "String",
-    age: Number,
-    isAdmin: Boolean,
+    firstName,
+    lastName,
+    age,
+    isAdmin,
   };
 }
 
 
 const newUser = createUser('Ola', 'Nordmann', 18);
 const newAdmin = createUser('Kari', 'Nordmann', 36, true);
+
+console.log(newUser)
 
